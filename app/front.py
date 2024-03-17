1cce6e1b9a864e0c36a509d7e85b4004a7477c3a
keitarods <83796605+keitarods@users.noreply.github.com>
2024-03-17 00:20:20 -0300

    Adicionada novas features - Preço médio, Quantidade e Soma acumulativa por ativo

--- app/front.py
+++ app/front.py
@@ -1,23 +1,59 @@
 import streamlit as st
 import os
-from leitura import leitura_arquivos, obtem_tickers
+from leitura import leitura_arquivos, obtem_tickers, agrupa_valor, remove_ticker
 from st_aggrid import AgGrid
 from streamlit_pandas_profiling import st_profile_report
+import plotly.express as px
+import altair as alt
+import pandas as pd
 
 st.write("""
 # Projeto de análise B3
 Essa é uma página voltada para verificar as suas movimentações na Bolsa B3
 """)
 
+#Realiza leitura dos arquivos
 df = leitura_arquivos("./data")
 
+#Removedor de ativos não desejados
+df = remove_ticker(df, list(["RBVA11", "ARCT11"]))
+
+#Header
 st.subheader("Extato de compra e venda de ativos")
+
+#Cria botão para selecionar filtro de ativos
 selecao= st.multiselect("Selecione", options = obtem_tickers(df))
 
 if len(selecao) > 0:
     df_select = df[df["Código de Negociação"].isin(selecao)]
+   
     st.dataframe(df_select)
 
 else:
     st.dataframe(df)
 
+#Cria os agrupamentos por preço médio, quantidade de ativos, e acumulo dos ativos pelo tempo
+df_agrupa = agrupa_valor(df)
+col1, col2 = st.columns(2) 
+with col1:
+    st.subheader("Preço Médio por ativo")
+    st.dataframe(df_agrupa[0])
+
+with col2:
+    st.subheader("Quantidade de ativos")
+    st.dataframe(df_agrupa[1])
+
+st.subheader("Soma acumulativa")
+st.dataframe(df_agrupa[2])
+
+# Cria o gráfico com a progressão dos ativos em função do tempo
+fig = px.line(df_agrupa[2], x='Data do Negócio', y='Valor', color='Código de Negociação',
+            title='Progressão dos Ativos em Função do Tempo',
+            labels={'Data do Negócio': 'Data', 'Valor': 'Valor'},
+            hover_data={'Data do Negócio': '|%B %d, %Y', 'Código de Negociação': True, 'Valor': ':.2f'})
+
+# Ajusta a aparência do gráfico
+fig.update_layout(width=1000, height=600)
+
+# Exibe o gráfico no Streamlit
+st.plotly_chart(fig, use_container_width=True)
--- app/leitura.py
+++ app/leitura.py
@@ -6,17 +6,30 @@ import openpyxl
 def leitura_arquivos(local):
     dfs = []
 
+    #Itera sobre a pasta para leitura dos arquivos
     for arquivo in os.listdir(local):
+        #Verifica se o arquivo é do formato excel
         if arquivo.endswith("xlsx"):
             local_file = os.path.join(local, arquivo)
             df = pd.read_excel(local_file)
-
             dfs.append(df)
 
-    df = pd.concat(dfs)
-    df = df.sort_values("Data do Negócio", ascending = True)
+    df = pd.concat(dfs).sort_values("Data do Negócio", ascending = True) #Ordena ativos por data
 
     return df
 
 def obtem_tickers(df):
-    return df["Código de Negociação"].unique()
+    tickers = df["Código de Negociação"].unique()
+    return tickers
+
+def agrupa_valor(df):
+    data = df.groupby(["Código de Negociação"])["Preço"].agg(Média_Preço=("mean"))
+    data_1 = df.groupby(["Código de Negociação"])["Quantidade"].agg(Qti=("sum"))
+    data_2 = df.set_index(["Código de Negociação", "Data do Negócio"])["Valor"].groupby(level=0).cumsum().reset_index()
+
+    return pd.DataFrame(data), pd.DataFrame(data_1), pd.DataFrame(data_2)
+
+def remove_ticker(df, ticker: list):
+    data = df[~df["Código de Negociação"].isin(ticker)]
+    
+    return data
